8444e7a4581510895ebbfbe8c2c00cc1b72af7a6
Warley Noleto <war.noleto@gmail.com>
2018-01-07 18:32:31 -0200

    adding action to register button

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
     <h1>Register</h1>
-    <input type="email" name="email" id="email" placeholder="email">
+    <input type="email" name="email" id="email" v-model="email" placeholder="email">
     <br>
-    <input type="password" name="password" id="password" placeholder="password">
+    <input type="password" name="password" id="password" v-model="password" placeholder="password">
     <br>
-    <button type="submit">Register</button> 
+    <button @click="register" type="submit">Register</button> 
   </div>
 </template>
 
 <script>
+
+import AuthenticationService from '@/services/AuthenticationService'
+
 export default {
   data () {
     return {
       email: '',
       password: ''
     }
+  },
+  methods: {
+    async register (event) {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
   }
+
 }
 </script>
 
