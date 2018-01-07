da2f13e7be2178edde0ed13bd0d4c8aff24c0b19
Warley Noleto <war.noleto@gmail.com>
2018-01-07 10:13:58 -0200

    formatting js code

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
 app.get('/status', (req, res) => {
   res.send({
     message: 'this server is perfectly running!'
   })
 })
+
 app.listen(process.env.PORT || 8081)
