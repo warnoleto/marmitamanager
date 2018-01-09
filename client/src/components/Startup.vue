61d6ebca050435cf537d4c02a073e3ad3da6639b
warnoleto <war.noleto@gmail.com>
2018-01-09 11:15:16 -0200

    playing with bootstrap components (thumbnails)

--- client/src/components/Startup.vue
+++ client/src/components/Startup.vue
@@ -1,21 +1,27 @@
 <template>
-  
-  <div class="jumbotron">
-    <div class="container">
-      <h1 class="display-3">Marmita Manager!</h1>
-      <p>Monte e peça sua marmita que entregaremos em sua casa.</p>
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">Saiba Mais »</a></p>
+  <div>
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">Marmita Manager!</h1>
+        <p>Monte e peça sua marmita que entregaremos em sua casa.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Saiba Mais »</a></p>
+      </div>
     </div>
+    <vbs-thumbnails></vbs-thumbnails>
   </div>
-
 </template>
 
 <script>
+import VbsThumbnails from '@/components/VbsThumbnails'
+
 export default {
   name: 'Startup',
   data () {
     return {
     }
+  },
+  components: {
+    'vbs-thumbnails': VbsThumbnails
   }
 }
 </script>
---
+++ client/src/components/VbsThumbnails.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container container-fluid">
+      <div class="col-xs-6 col-md-3" v-for="item in items" v-bind:key="item.title">
+          <div class="thumbnail">
+              <img :src="item.src" :alt="thumbnail-blank" >
+              <div class="caption">
+                <h3>{{item.title}}</h3>
+                <p>{{item.description}}</p>
+                <p><a href="#" class="btn btn-primary" role="button">Saiba mais.</a></p>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      items: [
+          {title: 'item1', description: 'description of item 1', src: './static/thumbnail_blank.png'},
+          {title: 'item2', description: 'description of item 2', src: './static/thumbnail_blank.png'},
+          {title: 'item3', description: 'description of item 3', src: './static/thumbnail_blank.png'},
+          {title: 'item4', description: 'description of item 4', src: './static/thumbnail_blank.png'},
+          {title: 'item5', description: 'description of item 5', src: './static/thumbnail_blank.png'},
+          {title: 'item6', description: 'description of item 6', src: './static/thumbnail_blank.png'},
+          {title: 'item7', description: 'description of item 7', src: './static/thumbnail_blank.png'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
