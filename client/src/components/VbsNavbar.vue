974652c924602fca9feda1e227192418c75485e3
warnoleto <war.noleto@gmail.com>
2018-02-25 21:08:59 -0300

    adjustments on navbar

--- client/src/components/VbsNavbar.vue
+++ client/src/components/VbsNavbar.vue
@@ -1,13 +1,36 @@
 <template>
   <navbar placement="top" type="inverse">
     <a slot="brand" href="/" title="Home" class="navbar-brand">{{brand}}</a>
-    <router-link tag="li" v-for="itm in commonRoutes" :key="itm.path"  :to="itm.path" active-class="active" :exact="true" :slot="itm.slot">
-      <a v-text="itm.label"></a>
+    
+    <router-link tag="li" to="/theplace" active-class="active" :exact="true">
+      <a>Instalações</a>
     </router-link>
 
-    <router-link tag="li" v-for="itm in userRoutes" :key="itm.path"  :to="itm.path" active-class="active" :exact="true" :slot="itm.slot">
-      <a v-text="itm.label" v-if="isNotAuthenticated"></a>
+    <router-link tag="li" to="/services" active-class="active" :exact="true">
+      <a>Serviços</a>
     </router-link>
+
+    <dropdown text="Administração" slot="right" v-if="isAuthenticated">
+      <router-link tag="li" to="/admin/category" active-class="active" :exact="true">
+        <a>Categorias</a>
+      </router-link>
+      <router-link tag="li" to="/admin/option" active-class="active" :exact="true">
+        <a>Opções</a>
+      </router-link>
+    </dropdown>
+
+    <router-link tag="li" to="/login" active-class="active" :exact="true" slot="right" v-if="isNotAuthenticated">
+      <a>Entrar</a>
+    </router-link>
+
+    <router-link tag="li" to="/register" active-class="active" :exact="true" slot="right" v-if="isNotAuthenticated">
+      <a>Registrar-se</a>
+    </router-link>
+
+    <li @click="logoff" slot="right" v-if="isAuthenticated">
+      <a>Sair</a>
+    </li>
+
   </navbar>
 </template>
 
@@ -34,6 +57,11 @@ export default {
     },
     isNotAuthenticated () {
       return !this.$store.getters.isAuthenticated
+    }
+  },
+  methods: {
+    logoff () {
+      this.$store.dispatch('setAuthenticationInfo', {})
     }
   }
 }
